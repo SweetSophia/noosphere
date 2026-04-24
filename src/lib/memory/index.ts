96816dd35051ef109a2002b418e0196e28bcfb9b
SweetSophia <mpreichhardt@gmail.com>
2026-04-24 23:28:04 +0200

    Add universal memory provider interface

--- src/lib/memory/index.ts
+++ src/lib/memory/index.ts
@@ -1,4 +1,15 @@
 export type {
+  MemoryProvider,
+  MemoryProviderCapabilities,
+  MemoryProviderConfig,
+  MemoryProviderDescriptor,
+  MemoryProviderGetOptions,
+  MemoryProviderScore,
+  MemoryProviderScoreContext,
+  MemoryProviderSearchOptions,
+} from "./provider";
+
+export type {
   MemoryCurationLevel,
   MemoryProviderMetadata,
   MemoryResult,
@@ -6,6 +17,12 @@ export type {
   MemoryScore,
   MemorySourceType,
 } from "./types";
+
+export {
+  DEFAULT_MEMORY_PROVIDER_CAPABILITIES,
+  DEFAULT_MEMORY_PROVIDER_CONFIG,
+  normalizeMemoryProviderConfig,
+} from "./provider";
 
 export {
   DEFAULT_MEMORY_CHARS_PER_TOKEN,
